2956901dbfdaf108e03c0404c188641de61b51fd
Papashii <patryh25@gmail.com>
2025-02-16 21:07:16 +0100

    Pliki projektu

--- Form4.Designer.cs
+++ Form4.Designer.cs
@@ -28,7 +28,53 @@
         /// </summary>
         private void InitializeComponent()
         {
+            dataGridView1 = new DataGridView();
+            label1 = new Label();
+            button1 = new Button();
+            Wydatki = new DataGridViewTextBoxColumn();
+            Przychody = new DataGridViewTextBoxColumn();
+            Reszta = new DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { Wydatki, Przychody, Reszta });
+            dataGridView1.Location = new Point(112, 170);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.Size = new Size(341, 150);
+            dataGridView1.TabIndex = 0;
+            dataGridView1.CellContentClick += dataGridView1_CellContentClick_1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 20F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.ForeColor = Color.Snow;
+            label1.Location = new Point(79, 59);
+            label1.Name = "label1";
+            label1.Size = new Size(468, 37);
+            label1.TabIndex = 1;
+            label1.Text = "Tabela ogólna wydatków/przychodów";
+            label1.Click += label1_Click;
+          
+            // 
+            // Wydatki
+            // 
+            Wydatki.HeaderText = "Wydatki";
+            Wydatki.Name = "Wydatki";
+            // 
+            // Przychody
+            // 
+            Przychody.HeaderText = "Przychody";
+            Przychody.Name = "Przychody";
+            // 
+            // Reszta
+            // 
+            Reszta.HeaderText = "Reszta";
+            Reszta.Name = "Reszta";
             // 
             // Form4
             // 
@@ -36,11 +82,24 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.HotTrack;
             ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Controls.Add(dataGridView1);
             Name = "Form4";
             Text = "Raport budżetu domowego";
+            Load += Form4_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private DataGridView dataGridView1;
+        private Label label1;
+        private Button button1;
+        private DataGridViewTextBoxColumn Wydatki;
+        private DataGridViewTextBoxColumn Przychody;
+        private DataGridViewTextBoxColumn Reszta;
     }
 }
--- Form4.cs
+++ Form4.cs
@@ -16,5 +16,53 @@ namespace Budzet_Domowy
         {
             InitializeComponent();
         }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Form4_Load(object sender, EventArgs e)
+        {
+            LoadDataFromFile("dane.txt");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonLoadData_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void LoadDataFromFile(string filePath)
+        {
+            try
+            {
+                var lines = File.ReadAllLines(filePath);
+                var data = lines.Select(line => line.Split(',')).ToList();
+
+                foreach (var row in data)
+                {
+
+                    int Przychód = int.Parse(row[0]);
+                    int Wydatki = int.Parse(row[1]);
+                    int Razem = Przychód - Wydatki;
+
+
+                    dataGridView1.Rows.Add(Przychód, Wydatki, Razem);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Błąd podczas odczytu pliku: {ex.Message}");
+            }
+        }
+
+        private void dataGridView1_CellContentClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
     }
 }
